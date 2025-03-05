be1709aff622c2ae3f8df89e1aa6bd2aa7943803
Basel Embaby <engbasel2025@gmail.com>
2025-03-05 02:47:24 +0200

    `Refactor CustomBlocObserver to use a formatted debugLog function`

--- lib/core/services/custom_bloc_observer.dart
+++ lib/core/services/custom_bloc_observer.dart
@@ -2,32 +2,40 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/foundation.dart';
 
 class CustomBlocObserver extends BlocObserver {
-  /// Utility function to print debug logs
-  void debugLog(String message) {
-    debugPrint('🔹 [Bloc Observer] $message');
+  /// Utility function to print formatted debug logs
+  void debugLog(String icon, String label, String message) {
+    debugPrint('''
+------------------------------------------------------------
+$icon [$label] 
+------------------------------------------------------------
+$message
+------------------------------------------------------------
+''');
   }
 
   @override
   void onEvent(Bloc bloc, Object? event) {
     super.onEvent(bloc, event);
-    debugLog('Bloc: ${bloc.runtimeType}, Event: $event');
+    debugLog('🟢', 'EVENT', 'Bloc: ${bloc.runtimeType}, Event: $event');
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
-    debugLog('Bloc: ${bloc.runtimeType}, Transition: $transition');
+    debugLog('🔄', 'TRANSITION',
+        'Bloc: ${bloc.runtimeType}, Transition: $transition');
   }
 
   @override
   void onChange(BlocBase bloc, Change change) {
     super.onChange(bloc, change);
-    debugLog('Bloc: ${bloc.runtimeType}, Change: $change');
+    debugLog('🔷', 'CHANGE', 'Bloc: ${bloc.runtimeType}, Change: $change');
   }
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    debugLog('❌ Bloc: ${bloc.runtimeType}, Error: $error');
+    debugLog('❌', 'ERROR',
+        'Bloc: ${bloc.runtimeType}, Error: $error\nStackTrace: $stackTrace');
     super.onError(bloc, error, stackTrace);
   }
 }
